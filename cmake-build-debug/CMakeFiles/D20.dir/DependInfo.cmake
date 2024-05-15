fa5c6a444226c919f1490346d6b7edc6a4e830fd
MHD Eyad Abou ker <126291554+EyadAbouKer@users.noreply.github.com>
2024-05-15 16:43:46 -0400

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/D20.dir/DependInfo.cmake
@@ -0,0 +1,47 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Admin/Desktop/Concordia/Past Terms/Winter2024/COMP_345_Advanced_Design_IN_C++/Dungeons and Dragons D20 Game/src/arena/combatarena/combatarena.cpp" "CMakeFiles/D20.dir/src/arena/combatarena/combatarena.cpp.obj" "gcc" "CMakeFiles/D20.dir/src/arena/combatarena/combatarena.cpp.obj.d"
+  "C:/Users/Admin/Desktop/Concordia/Past Terms/Winter2024/COMP_345_Advanced_Design_IN_C++/Dungeons and Dragons D20 Game/src/arena/friendarena/friendarena.cpp" "CMakeFiles/D20.dir/src/arena/friendarena/friendarena.cpp.obj" "gcc" "CMakeFiles/D20.dir/src/arena/friendarena/friendarena.cpp.obj.d"
+  "C:/Users/Admin/Desktop/Concordia/Past Terms/Winter2024/COMP_345_Advanced_Design_IN_C++/Dungeons and Dragons D20 Game/src/characters/character/character.cpp" "CMakeFiles/D20.dir/src/characters/character/character.cpp.obj" "gcc" "CMakeFiles/D20.dir/src/characters/character/character.cpp.obj.d"
+  "C:/Users/Admin/Desktop/Concordia/Past Terms/Winter2024/COMP_345_Advanced_Design_IN_C++/Dungeons and Dragons D20 Game/src/characters/character/characterObserverPattern/CharacterObserver.cpp" "CMakeFiles/D20.dir/src/characters/character/characterObserverPattern/CharacterObserver.cpp.obj" "gcc" "CMakeFiles/D20.dir/src/characters/character/characterObserverPattern/CharacterObserver.cpp.obj.d"
+  "C:/Users/Admin/Desktop/Concordia/Past Terms/Winter2024/COMP_345_Advanced_Design_IN_C++/Dungeons and Dragons D20 Game/src/characters/character/friend.cpp" "CMakeFiles/D20.dir/src/characters/character/friend.cpp.obj" "gcc" "CMakeFiles/D20.dir/src/characters/character/friend.cpp.obj.d"
+  "C:/Users/Admin/Desktop/Concordia/Past Terms/Winter2024/COMP_345_Advanced_Design_IN_C++/Dungeons and Dragons D20 Game/src/characters/character/itemcontainer.cpp" "CMakeFiles/D20.dir/src/characters/character/itemcontainer.cpp.obj" "gcc" "CMakeFiles/D20.dir/src/characters/character/itemcontainer.cpp.obj.d"
+  "C:/Users/Admin/Desktop/Concordia/Past Terms/Winter2024/COMP_345_Advanced_Design_IN_C++/Dungeons and Dragons D20 Game/src/characters/enemy/enemieBuilder/creeps/creeps.cpp" "CMakeFiles/D20.dir/src/characters/enemy/enemieBuilder/creeps/creeps.cpp.obj" "gcc" "CMakeFiles/D20.dir/src/characters/enemy/enemieBuilder/creeps/creeps.cpp.obj.d"
+  "C:/Users/Admin/Desktop/Concordia/Past Terms/Winter2024/COMP_345_Advanced_Design_IN_C++/Dungeons and Dragons D20 Game/src/characters/enemy/enemieBuilder/titan/titan.cpp" "CMakeFiles/D20.dir/src/characters/enemy/enemieBuilder/titan/titan.cpp.obj" "gcc" "CMakeFiles/D20.dir/src/characters/enemy/enemieBuilder/titan/titan.cpp.obj.d"
+  "C:/Users/Admin/Desktop/Concordia/Past Terms/Winter2024/COMP_345_Advanced_Design_IN_C++/Dungeons and Dragons D20 Game/src/characters/enemy/enemy.cpp" "CMakeFiles/D20.dir/src/characters/enemy/enemy.cpp.obj" "gcc" "CMakeFiles/D20.dir/src/characters/enemy/enemy.cpp.obj.d"
+  "C:/Users/Admin/Desktop/Concordia/Past Terms/Winter2024/COMP_345_Advanced_Design_IN_C++/Dungeons and Dragons D20 Game/src/characters/player/player.cpp" "CMakeFiles/D20.dir/src/characters/player/player.cpp.obj" "gcc" "CMakeFiles/D20.dir/src/characters/player/player.cpp.obj.d"
+  "C:/Users/Admin/Desktop/Concordia/Past Terms/Winter2024/COMP_345_Advanced_Design_IN_C++/Dungeons and Dragons D20 Game/src/characters/player/playerBuilder/builtPlayer.cpp" "CMakeFiles/D20.dir/src/characters/player/playerBuilder/builtPlayer.cpp.obj" "gcc" "CMakeFiles/D20.dir/src/characters/player/playerBuilder/builtPlayer.cpp.obj.d"
+  "C:/Users/Admin/Desktop/Concordia/Past Terms/Winter2024/COMP_345_Advanced_Design_IN_C++/Dungeons and Dragons D20 Game/src/characters/player/players/abstract_character_builder.cpp" "CMakeFiles/D20.dir/src/characters/player/players/abstract_character_builder.cpp.obj" "gcc" "CMakeFiles/D20.dir/src/characters/player/players/abstract_character_builder.cpp.obj.d"
+  "C:/Users/Admin/Desktop/Concordia/Past Terms/Winter2024/COMP_345_Advanced_Design_IN_C++/Dungeons and Dragons D20 Game/src/characters/player/players/assasin/assasin.cpp" "CMakeFiles/D20.dir/src/characters/player/players/assasin/assasin.cpp.obj" "gcc" "CMakeFiles/D20.dir/src/characters/player/players/assasin/assasin.cpp.obj.d"
+  "C:/Users/Admin/Desktop/Concordia/Past Terms/Winter2024/COMP_345_Advanced_Design_IN_C++/Dungeons and Dragons D20 Game/src/characters/player/players/axe/axe.cpp" "CMakeFiles/D20.dir/src/characters/player/players/axe/axe.cpp.obj" "gcc" "CMakeFiles/D20.dir/src/characters/player/players/axe/axe.cpp.obj.d"
+  "C:/Users/Admin/Desktop/Concordia/Past Terms/Winter2024/COMP_345_Advanced_Design_IN_C++/Dungeons and Dragons D20 Game/src/characters/player/players/bloodSeeker/bloodSeeker.cpp" "CMakeFiles/D20.dir/src/characters/player/players/bloodSeeker/bloodSeeker.cpp.obj" "gcc" "CMakeFiles/D20.dir/src/characters/player/players/bloodSeeker/bloodSeeker.cpp.obj.d"
+  "C:/Users/Admin/Desktop/Concordia/Past Terms/Winter2024/COMP_345_Advanced_Design_IN_C++/Dungeons and Dragons D20 Game/src/characters/player/players/spiritBreaker/spiritBreaker.cpp" "CMakeFiles/D20.dir/src/characters/player/players/spiritBreaker/spiritBreaker.cpp.obj" "gcc" "CMakeFiles/D20.dir/src/characters/player/players/spiritBreaker/spiritBreaker.cpp.obj.d"
+  "C:/Users/Admin/Desktop/Concordia/Past Terms/Winter2024/COMP_345_Advanced_Design_IN_C++/Dungeons and Dragons D20 Game/src/controllers/gameController/gameController.cpp" "CMakeFiles/D20.dir/src/controllers/gameController/gameController.cpp.obj" "gcc" "CMakeFiles/D20.dir/src/controllers/gameController/gameController.cpp.obj.d"
+  "C:/Users/Admin/Desktop/Concordia/Past Terms/Winter2024/COMP_345_Advanced_Design_IN_C++/Dungeons and Dragons D20 Game/src/dice/dice.cpp" "CMakeFiles/D20.dir/src/dice/dice.cpp.obj" "gcc" "CMakeFiles/D20.dir/src/dice/dice.cpp.obj.d"
+  "C:/Users/Admin/Desktop/Concordia/Past Terms/Winter2024/COMP_345_Advanced_Design_IN_C++/Dungeons and Dragons D20 Game/src/donut.cpp" "CMakeFiles/D20.dir/src/donut.cpp.obj" "gcc" "CMakeFiles/D20.dir/src/donut.cpp.obj.d"
+  "C:/Users/Admin/Desktop/Concordia/Past Terms/Winter2024/COMP_345_Advanced_Design_IN_C++/Dungeons and Dragons D20 Game/src/items/equipment/equipment.cpp" "CMakeFiles/D20.dir/src/items/equipment/equipment.cpp.obj" "gcc" "CMakeFiles/D20.dir/src/items/equipment/equipment.cpp.obj.d"
+  "C:/Users/Admin/Desktop/Concordia/Past Terms/Winter2024/COMP_345_Advanced_Design_IN_C++/Dungeons and Dragons D20 Game/src/items/item/item.cpp" "CMakeFiles/D20.dir/src/items/item/item.cpp.obj" "gcc" "CMakeFiles/D20.dir/src/items/item/item.cpp.obj.d"
+  "C:/Users/Admin/Desktop/Concordia/Past Terms/Winter2024/COMP_345_Advanced_Design_IN_C++/Dungeons and Dragons D20 Game/src/logs/logWriter.cpp" "CMakeFiles/D20.dir/src/logs/logWriter.cpp.obj" "gcc" "CMakeFiles/D20.dir/src/logs/logWriter.cpp.obj.d"
+  "C:/Users/Admin/Desktop/Concordia/Past Terms/Winter2024/COMP_345_Advanced_Design_IN_C++/Dungeons and Dragons D20 Game/src/main.cpp" "CMakeFiles/D20.dir/src/main.cpp.obj" "gcc" "CMakeFiles/D20.dir/src/main.cpp.obj.d"
+  "C:/Users/Admin/Desktop/Concordia/Past Terms/Winter2024/COMP_345_Advanced_Design_IN_C++/Dungeons and Dragons D20 Game/src/maps/campaign/campaign.cpp" "CMakeFiles/D20.dir/src/maps/campaign/campaign.cpp.obj" "gcc" "CMakeFiles/D20.dir/src/maps/campaign/campaign.cpp.obj.d"
+  "C:/Users/Admin/Desktop/Concordia/Past Terms/Winter2024/COMP_345_Advanced_Design_IN_C++/Dungeons and Dragons D20 Game/src/maps/map/map.cpp" "CMakeFiles/D20.dir/src/maps/map/map.cpp.obj" "gcc" "CMakeFiles/D20.dir/src/maps/map/map.cpp.obj.d"
+  "C:/Users/Admin/Desktop/Concordia/Past Terms/Winter2024/COMP_345_Advanced_Design_IN_C++/Dungeons and Dragons D20 Game/src/renderEngine/gameView/gameView.cpp" "CMakeFiles/D20.dir/src/renderEngine/gameView/gameView.cpp.obj" "gcc" "CMakeFiles/D20.dir/src/renderEngine/gameView/gameView.cpp.obj.d"
+  "C:/Users/Admin/Desktop/Concordia/Past Terms/Winter2024/COMP_345_Advanced_Design_IN_C++/Dungeons and Dragons D20 Game/src/stats/abilityscores/abilityscores.cpp" "CMakeFiles/D20.dir/src/stats/abilityscores/abilityscores.cpp.obj" "gcc" "CMakeFiles/D20.dir/src/stats/abilityscores/abilityscores.cpp.obj.d"
+  "C:/Users/Admin/Desktop/Concordia/Past Terms/Winter2024/COMP_345_Advanced_Design_IN_C++/Dungeons and Dragons D20 Game/src/stats/hp/hp.cpp" "CMakeFiles/D20.dir/src/stats/hp/hp.cpp.obj" "gcc" "CMakeFiles/D20.dir/src/stats/hp/hp.cpp.obj.d"
+  "C:/Users/Admin/Desktop/Concordia/Past Terms/Winter2024/COMP_345_Advanced_Design_IN_C++/Dungeons and Dragons D20 Game/src/stats/levelsystem/levelsystem.cpp" "CMakeFiles/D20.dir/src/stats/levelsystem/levelsystem.cpp.obj" "gcc" "CMakeFiles/D20.dir/src/stats/levelsystem/levelsystem.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
